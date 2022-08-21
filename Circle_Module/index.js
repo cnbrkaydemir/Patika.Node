06e476326ecfc5e44b3c9cdd2ed51dfd3a865e51
Canberk Aydemir <canberk.aydemir@yahoo.com>
2022-08-21 21:52:38 +0300

    Compelete node.js modules project.

---
+++ Circle_Module/index.js
@@ -0,0 +1,9 @@
+export const circleArea = (radius) => {
+    let area = (Math.PI * Math.pow(radius, 2)).toFixed(1);
+    console.log(`Area of circle with ${radius} is : ${area}`);
+  };
+
+  export const circleCircumference = (radius) => {
+    let circumference = (Math.PI * 2 * radius).toFixed(1);
+    console.log(`Circumference ofYarıçapı ${radius} olan dairenin çevresi: ${circumference}`);
+  };
